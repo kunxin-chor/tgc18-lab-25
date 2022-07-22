550132f45b9de5f966ca88569b12c087be0593dd
Paul <chorkunxin@gmail.com>
2022-07-22 16:33:02 +0800

    Added handlebars helpers

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const hbs = require('hbs')
 const wax = require('wax-on');
-
+var helpers = require('handlebars-helpers')({
+    handlebars: hbs.handlebars
+  });
+  
 const app = express();
 
 app.set('view engine', 'hbs');
